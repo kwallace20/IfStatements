61efe7d15163f7173ed04680ec33726e16d23a38
kwallace20 <43682545+kwallace20@users.noreply.github.com>
2018-11-29 12:32:41 -0600

    Add files via upload

---
+++ else.py
@@ -0,0 +1,11 @@
+import RoboPiLib as RPL
+import setup
+x = 1
+sensor_pin = 16
+RPL.pinMode(sensor_pin,RPL.INPUT)
+while x == 1:
+	reading = RPL.digitalRead(sensor_pin)
+	if reading == 1:
+		RPL.servoWrite(0, 1590)
+	else:
+		RPL.servoWrite(0, 1410)
