d53acde312627b4584a2fac7162148446c64b480
kwallace20 <43682545+kwallace20@users.noreply.github.com>
2018-11-29 11:37:52 -0600

    Add files via upload

---
+++ and.py
@@ -0,0 +1,12 @@
+import RoboPiLib as RoboPiLib
+import setup
+x = 1
+RPL.pinMode(16,RPL.INPUT)
+RPL.pinMode(17,RPL.INPUT)
+while x == 1:
+	reading16 = RPL.digitalRead(16)
+	reading17 = RPL.digitalRead(17)
+	if reading16 == 0 and reading17 == 0:
+		RPL.servoWrite(0, 1590)
+	elif reading16 == 1 and reading17 == 1:
+		RPL.servoWrite(0, 1500)
